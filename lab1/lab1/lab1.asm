c61831386bd864f51eb1ee9ec54094fdb7fd11c1
lincl896 <linus.classon@hotmail.com>
2019-11-13 15:30:47 +0100

    push!

--- lab1/lab1/lab1.asm
+++ lab1/lab1/lab1.asm
@@ -5,4 +5,35 @@
  *   Author: lincl896
  */ 
 
+INIT:
+	ldi r25,$ff
+	out DDRB,r15
+	ldi r25,$00
+	out DDRA, r15
 
+
+IDLE:
+	breq GET_DATA
+	jmp IDLE
+
+
+DELAY:
+	sbi PORTB,7
+	ldi r16,10
+DelayYttreLoop:
+	ldi r17,$1F
+DelayInreLoop:
+	dec r17
+	brne DelayInreLoop
+	dec r16;
+	brne DelayYttreLoop
+	cbi PORTB,7
+	ret
+
+GET_DATA:
+	clr r19
+	in r19,PINA0
+	Call DELAY
+	brne GET_DATA
+	out PORTB,r19
+	ret
