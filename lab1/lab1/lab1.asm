d2fc8db47e2b8fba28796a897d3a89d99f80c277
Linus Classon <linus.classon@hotmail.com>
2019-11-14 18:46:13 +0100

    Lab 1 Done

--- lab1/lab1/lab1.asm
+++ lab1/lab1/lab1.asm
@@ -5,29 +5,45 @@
  *   Author: lincl896
  */ 
 
-INIT:
+SETUP:
+	;ställer in IO Pinnar och rensar register
 	ldi r25,HIGH(RAMEND); set stack
 	out SPH,r25; for calls
 	ldi r25,LOW(RAMEND);
 	out SPL,r25
 
-	ldi r25,$ff
+	ldi r25,$8f
 	out DDRB,r25
 	ldi r25,$00
 	out DDRA, r25
-	ldi r21,$04; räknare
 	clr r20 ; nummer
-	clr r22 ; delay eller inte 1/0
+	ldi r23,$08
+
+MAIN:
+	call INIT
+	call IDLE
+	call PRINT
+	jmp MAIN
+
+INIT:
+	;ställer in register
+	ldi r21,$04; räknare
+	ldi r16,10
+	clr r22
+	ret
 
 IDLE:
 	sbis PINA,0
 	jmp IDLE
-	jmp GET_DATA
+	call DELAY
+	sbic PINA,0
+	call GET_DATA
+	ret
 
 
 DELAY:
 	sbi PORTB,7
-	ldi r16,10
+	//ldi r16,10
 DelayYttreLoop:
 	ldi r17,$1F
 DelayInreLoop:
@@ -36,18 +52,28 @@ DelayInreLoop:
 	dec r16;
 	brne DelayYttreLoop
 	cbi PORTB,7
-	inc r22;
+	ret
 
 GET_DATA:
-	sbrs r22,0
+	ldi r16,20
 	call DELAY
-	dec r22
-	in r19,PINA0
-	add r20,r19
+	in r19,PINA
+	call SORT_DATA
 	dec r21
-	breq PRINT
-	jmp GET_DATA
+	brne GET_DATA
+	ret
+
+SORT_DATA:
+	andi r19,$01
+	lsr r22
+	sbrc r19,0
+	add r22,r23
+	ret
+
 
 PRINT:
+	ldi r16,20
+	call DELAY
+	mov r20,r22
 	out PORTB,r20
-	jmp IDLE
+	ret
