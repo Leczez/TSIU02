4765fdfcf8dea0dd491dda3db6eb2702add4fba3
Linus Classon <linus.classon@hotmail.com>
2019-12-06 11:19:27 +0100

    done some work

--- lab3/lab3/lab3.asm
+++ lab3/lab3/lab3.asm
@@ -6,3 +6,51 @@
  */ 
 
 
+ SETUP:
+
+	ldi r16,HIGH(RAMEND)
+	out SPH,r16
+	ldi r16,LOW(RAMEND)
+	out SPL,r16
+
+	ldi r16,$FF
+	out DDRB,r16
+	ldi r16,$07
+	out DDRA,r16
+
+
+
+	sei
+
+MAIN:
+	sleep
+	rjmp MAIN
+
+
+ MUX:
+
+	ret ; kanske reti
+
+
+INTERRUPT1:
+	rcall SAVE_STATUS_REGISTER
+
+	rcall RESTORE_STATUS_REGISTER
+	reti
+
+
+INTERRUPT0:
+	rcall SAVE_STATUS_REGISTER
+
+	rcall RESTORE_STATUS_REGISTER
+	reti
+
+SAVE_STATUS_REGISTER:
+	push r16
+	in r16,SREG
+	ret
+
+RESTORE_STATUS_REGISTER:
+	out SREG,r16
+	pop r16
+	ret
